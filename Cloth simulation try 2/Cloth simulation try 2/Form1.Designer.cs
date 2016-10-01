9bc2ac327df5bdbe2c632596a72ddca96c1f79db
SimonMacLean <simon@teammaclean.net>
2016-10-01 15:09:49 -0700

    Added VS C# files

---
+++ Cloth simulation try 2/Cloth simulation try 2/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿using System.Windows.Forms;
+
+namespace Cloth_simulation_try_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pointsCheckbox = new System.Windows.Forms.RadioButton();
+            this.gridCheckbox = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.widthTextbox = new System.Windows.Forms.TextBox();
+            this.heightTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gravitySlider = new System.Windows.Forms.TrackBar();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gravitySlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this._Paint);
+            this.splitContainer1.Panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this._MouseDown);
+            this.splitContainer1.Panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this._MouseMove);
+            this.splitContainer1.Panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this._MouseUp);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.pointsCheckbox);
+            this.splitContainer1.Panel2.Controls.Add(this.gridCheckbox);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.widthTextbox);
+            this.splitContainer1.Panel2.Controls.Add(this.heightTextbox);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.gravitySlider);
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Size = new System.Drawing.Size(764, 612);
+            this.splitContainer1.SplitterDistance = 534;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Change background color...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.backgroundColorButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Change color...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.backgroundColorButton_Click);
+            // 
+            // pointsCheckbox
+            // 
+            this.pointsCheckbox.AutoSize = true;
+            this.pointsCheckbox.Location = new System.Drawing.Point(11, 146);
+            this.pointsCheckbox.Name = "pointsCheckbox";
+            this.pointsCheckbox.Size = new System.Drawing.Size(54, 17);
+            this.pointsCheckbox.TabIndex = 8;
+            this.pointsCheckbox.Text = "Points";
+            this.pointsCheckbox.UseVisualStyleBackColor = true;
+            this.pointsCheckbox.CheckedChanged += new System.EventHandler(this.pointsCheckbox_CheckedChanged);
+            // 
+            // gridCheckbox
+            // 
+            this.gridCheckbox.AutoSize = true;
+            this.gridCheckbox.Checked = true;
+            this.gridCheckbox.Location = new System.Drawing.Point(11, 169);
+            this.gridCheckbox.Name = "gridCheckbox";
+            this.gridCheckbox.Size = new System.Drawing.Size(65, 17);
+            this.gridCheckbox.TabIndex = 7;
+            this.gridCheckbox.TabStop = true;
+            this.gridCheckbox.Text = "Gridlines";
+            this.gridCheckbox.UseVisualStyleBackColor = true;
+            this.gridCheckbox.CheckedChanged += new System.EventHandler(this.gridCheckbox_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Height";
+            // 
+            // widthTextbox
+            // 
+            this.widthTextbox.Location = new System.Drawing.Point(112, 110);
+            this.widthTextbox.Name = "widthTextbox";
+            this.widthTextbox.Size = new System.Drawing.Size(100, 20);
+            this.widthTextbox.TabIndex = 4;
+            this.widthTextbox.TextChanged += new System.EventHandler(this.lengthChanged);
+            // 
+            // heightTextbox
+            // 
+            this.heightTextbox.Location = new System.Drawing.Point(6, 110);
+            this.heightTextbox.Name = "heightTextbox";
+            this.heightTextbox.Size = new System.Drawing.Size(100, 20);
+            this.heightTextbox.TabIndex = 3;
+            this.heightTextbox.TextChanged += new System.EventHandler(this.widthChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gravity";
+            // 
+            // gravitySlider
+            // 
+            this.gravitySlider.LargeChange = 1;
+            this.gravitySlider.Location = new System.Drawing.Point(6, 25);
+            this.gravitySlider.Maximum = 40;
+            this.gravitySlider.Name = "gravitySlider";
+            this.gravitySlider.Size = new System.Drawing.Size(206, 45);
+            this.gravitySlider.TabIndex = 0;
+            this.gravitySlider.Value = 4;
+            this.gravitySlider.Scroll += new System.EventHandler(this.gravitySlider_Scroll);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 612);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this._Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this._MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this._MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this._MouseUp);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gravitySlider)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar gravitySlider;
+        private Label label3;
+        private Label label2;
+        private TextBox widthTextbox;
+        private TextBox heightTextbox;
+        private RadioButton pointsCheckbox;
+        private RadioButton gridCheckbox;
+        private ColorDialog colorDialog1;
+        private Button button1;
+        private Button button2;
+    }
+}
+
---
+++ Cloth simulation try 2/Cloth simulation try 2/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cloth_simulation_try_2
+{
+    public partial class Form1 : Form
+    {
+
+        private int[] clothDimentions = { 20, 20 }; //size of cloth in points
+        private clothPoint[][] clothpoints;            //Array of points on cloth
+        private static double gravityY = 1;             //downward force of gravity. On earth it is 1
+        private static double gravityX = 0;             //force of gravity to the right. On earth it is 0
+        private const int UPI = 20;                     //Units per inch
+        public static int PPU = 200 / UPI;              //Pixels per unit
+        private static int padding = 60;                //Extra space from corners
+        double stretch = 2;
+        Timer drawTimer = new Timer();
+        Timer clickTimer = new Timer();
+        private int[] closestpoint;
+        Point mouse;
+        Color backgroundColor = Color.Black;
+        Color clothColor = Color.White;
+        public Form1()
+        {
+            InitializeComponent();
+            clickTimer.Interval = 1;
+            clickTimer.Tick += ClickTimer_Tick;
+            drawTimer.Interval = 1;
+            drawTimer.Enabled = true;
+            drawTimer.Tick += DrawTimer_Tick;
+            gravityY /= 2;
+            gravityX /= 2;
+            clothDimentions[0]++;
+            clothDimentions[1]++;
+            Make();
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty
+            | BindingFlags.Instance | BindingFlags.NonPublic, null,
+            splitContainer1.Panel1, new object[] { true });
+        }
+
+        private void ClickTimer_Tick(object sender, EventArgs e)
+        {
+            clothPoint p = clothpoints[closestpoint[0]][closestpoint[1]];
+                p.prevX = p.X;
+                p.prevY = p.Y;
+                p.X = mouse.X;
+                p.Y = mouse.Y;
+                clothpoints[closestpoint[0]][closestpoint[1]] = p;
+        }
+
+        private void DrawTimer_Tick(object sender, EventArgs e)
+        {
+            splitContainer1.Panel1.Invalidate();
+        }
+
+        private void Make()
+        {
+            clothpoints = new clothPoint[clothDimentions[1]][];
+            for (int i = 0; i < clothDimentions[1]; i++)
+            {
+                clothpoints[i] = new clothPoint[clothDimentions[0]];
+                for (int j = 0; j < clothDimentions[0]; j++)
+                {
+                    bool isstatic = j == 0;
+                    clothpoints[i][j] = new clothPoint(i * (PPU + (isstatic ? stretch : 0)) + padding, j * (PPU + (isstatic ? stretch : 0)) + padding, padding, isstatic);
+                }
+            }
+        }
+        private void updatePoints()
+        {
+            for (int i = 0; i < clothDimentions[1]; i++)
+            {
+                for (int j = 0; j < clothDimentions[0]; j++)
+                {
+                    var point = clothpoints[i][j];
+                    if (point.isStatic)
+                        continue;
+                    double velX = point.X - point.prevX;
+                    double velY = point.Y - point.prevY;
+                    point.prevX = point.X;
+                    point.prevY = point.Y;
+                    point.X += (velX + gravityX) * 0.955;
+                    point.Y += (velY + gravityY) * 0.955;
+                    //var abovePoint = clothpoints[(i - 1) * clothDimentions[0] + j];
+                    //var rightPoint = clothpoints[i * clothDimentions[0] + j + 1];
+                    //var belowPoint = clothpoints[(i + 1) * clothDimentions[0] + j];
+                    //var leftPoint = clothpoints[i * clothDimentions[0] + j - 1];
+                    List<clothPoint> nearbyPoints = new List<clothPoint>(4)
+                    { null, null, null, null};
+                    if (i > 0)
+                    {
+                        nearbyPoints[0] = clothpoints[i - 1][j];
+                    }
+                    if (j + 1 < clothDimentions[0])
+                    {
+                        nearbyPoints[1] = clothpoints[i][j + 1];
+                    }
+                    if (i + 1 < clothDimentions[1])
+                    {
+                        nearbyPoints[2] = clothpoints[i + 1][j];
+                    }
+                    if (j > 0)
+                    {
+                        nearbyPoints[3] = clothpoints[i][j - 1];
+                    }
+                    for (int k = 0; k < nearbyPoints.Count; k++)
+                    {
+                        if (nearbyPoints[k] == null)
+                            continue;
+                        double diffX = point.X - nearbyPoints[k].X;
+                        double diffY = point.Y - nearbyPoints[k].Y;
+                        var sqrDist = diffX * diffX + diffY * diffY;
+                        if (sqrDist > PPU * PPU)
+                        {
+                            double diff = Math.Sqrt(sqrDist);
+                            double difference = diff != 0 ? (PPU - diff) / diff : 0;
+                            double translateX = diffX * 0.48 * difference;
+                            double translateY = diffY * 0.48 * difference;
+                            if (!nearbyPoints[k].isStatic)
+                            {
+                                point.X += translateX;
+                                point.Y += translateY;
+                                nearbyPoints[k].X -= translateX;
+                                nearbyPoints[k].Y -= translateY;
+                            }
+                            else
+                            {
+                                point.X += 2 * translateX;
+                                point.Y += 2 * translateY;
+                            }
+                        }
+                    }
+                    clothpoints[i][j] = point;
+                    if (nearbyPoints[0] != null)
+                        clothpoints[i - 1][j] = nearbyPoints[0];
+                    if (nearbyPoints[1] != null)
+                        clothpoints[i][j + 1] = nearbyPoints[1];
+                    if (nearbyPoints[2] != null)
+                        clothpoints[i + 1][j] = nearbyPoints[2];
+                    if (nearbyPoints[3] != null)
+                        clothpoints[i][j - 1] = nearbyPoints[3];
+                }
+            }
+        }
+        //private void draw()
+        //{
+        //    canvas.Dispose();
+        //    canvas = new Bitmap(10000, 10000);
+        //    for (int i = 0; i < clothDimentions[1]; i++)
+        //    {
+        //        for (int j = 0; j < clothDimentions[0]; j++)
+        //        {
+        //            clothPoint t = clothpoints[i * clothDimentions[0] + j];
+
+        //            //if (t.X > 0 && t.Y > 0)
+        //            //    canvas.SetPixel((int)Math.Abs(t.X), (int)Math.Abs(t.Y), Color.Black);
+        //        }
+        //    }
+        //}
+
+        private void _Paint(object sender, PaintEventArgs e)
+        {
+            updatePoints();
+            e.Graphics.FillRectangle(new SolidBrush(backgroundColor), ClientRectangle);
+            for (int i = 0; i < clothDimentions[1]; i++)
+            {
+                for (int j = 0; j < clothDimentions[0]; j++)
+                {
+                    clothPoint t = clothpoints[i][j];
+                    if (gridCheckbox.Checked)
+                    {
+                        if (j + 1 < clothDimentions[0])
+                        {
+                            e.Graphics.DrawLine(new Pen(clothColor), (int)t.X, (int)t.Y, (int)clothpoints[i][j + 1].X, (int)clothpoints[i][j + 1].Y);
+                        }
+                        if (i + 1 < clothDimentions[1])
+                        {
+                            e.Graphics.DrawLine(new Pen(clothColor), (int)t.X, (int)t.Y, (int)clothpoints[i + 1][j].X, (int)clothpoints[i + 1][j].Y);
+                        }
+                    }
+                    else if (pointsCheckbox.Checked)
+                    {
+                            e.Graphics.FillEllipse(new SolidBrush(clothColor), t.getPointF().X, t.getPointF().Y/*(int)clothpoints[i][j].X, (int)clothpoints[i][j].Y*/, 2, 2);
+                    }
+                }
+            }
+        }
+
+        private void _MouseDown(object sender, MouseEventArgs e)
+        {
+            clickTimer.Enabled = true;
+            mouse = e.Location;
+
+            double distance = 100000;
+            for (int i = 0; i < clothDimentions[1]; i++)
+            {
+                for (int j = 0; j < clothDimentions[0]; j++)
+                {
+                    clothPoint t = clothpoints[i][j];
+                    if (t.distanceFrom(mouse) < distance && !t.isStatic)
+                    {
+                        distance = t.distanceFrom(mouse);
+                        closestpoint = new[] { i, j };
+                    }
+                }
+            }
+        }
+
+        private void _MouseUp(object sender, MouseEventArgs e)
+        {
+            clickTimer.Enabled = false;
+        }
+
+        private void _MouseMove(object sender, MouseEventArgs e)
+        {
+            mouse = e.Location;
+        }
+
+        private void lengthChanged(object sender, EventArgs e)
+        {
+            int value;
+            if (!int.TryParse(widthTextbox.Text, out value))
+            {
+                widthTextbox.Text = string.Empty;
+                widthTextbox.Focus();
+                return;
+            }
+            clothDimentions[1] = value + 1;
+            Make();
+        }
+
+        private void widthChanged(object sender, EventArgs e)
+        {
+            int value;
+            if (!int.TryParse(heightTextbox.Text, out value))
+            {
+                heightTextbox.Text = string.Empty;
+                heightTextbox.Focus();
+                return;
+            }
+            clothDimentions[0] = value + 1;
+            Make();
+        }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            clothColor = colorDialog1.Color;
+        }
+
+        private void backgroundColorButton_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            backgroundColor = colorDialog1.Color;
+        }
+
+        private void pointsCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (pointsCheckbox.Checked)
+            {
+                gridCheckbox.Checked = false;
+            }
+        }
+
+        private void gridCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (gridCheckbox.Checked)
+            {
+                pointsCheckbox.Checked = false;
+            }
+        }
+
+        private void gravitySlider_Scroll(object sender, EventArgs e)
+        {
+            gravityY = gravitySlider.Value / 8.0;
+        }
+        
+        }
+    }
+    public class clothPoint
+    {
+        public double X;
+        public double Y;
+        public double prevX;
+        public double prevY;
+        public bool isStatic;
+        public clothPoint(double x, double y, double z, bool isStatic)
+        {
+            this.isStatic = isStatic;
+            X = prevX = x;
+            Y = prevY = y;
+        }
+        public double distanceFrom(clothPoint other_point)
+        {
+            return Math.Sqrt(Math.Pow(other_point.X - this.X, 2) + Math.Pow(other_point.Y - this.Y, 2));
+        }
+        public double distanceFrom(Point other_point)
+        {
+            return Math.Sqrt(Math.Pow(other_point.X - this.X, 2) + Math.Pow(other_point.Y - this.Y, 2));
+        }
+        public PointF getPointF()
+        {
+            return new PointF((int)X, (int)Y);
+        }
+
+    }
